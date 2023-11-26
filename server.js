1682f9da693a153713e9447caa073c0fe81d3bc6
Mayukh <mayukhsasmal1999@gmail.com>
2023-11-26 23:07:25 +0530

    basic-setup

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//setup view engine
+app.set('view engine', 'ejs');
+
 //homepage route
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 //re-direct to homepage
@@ -12,9 +15,18 @@ app.get('/home', (req, res) => {
     res.redirect('/');
 });
 
+//about page route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
 //404 page - this must go at the end of the file
 app.get('*', (req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
 
 app.listen(port, () => {
